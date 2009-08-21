159f8b50a0fabae0bd7d1af7faaf2f2cde9387be
Rob Clark <rob@ti.com>
2009-08-21 17:33:07 -0500

    Use libtoolize rather than including ltmain.sh.
    This should fix problems building on Gentoo, or other platforms with different libtool from what is used on Ubuntu.

--- system/lcml/bootstrap.sh
+++ system/lcml/bootstrap.sh
@@ -2,5 +2,16 @@
 
 cd `dirname $0`
 
-mkdir -p config && aclocal && automake --copy --add-missing --foreign && autoconf
+# on some platforms, you have "g" versions of some of these tools instead,
+# ie glibtoolize instead of libtoolize..
+find_tool() {
+	which $1 2> /dev/null || which g$1 2> /dev/null
+}
+
+aclocal=`find_tool aclocal`
+libtoolize=`find_tool libtoolize`
+automake=`find_tool automake`
+autoconf=`find_tool autoconf`
+
+mkdir -p config && $aclocal && $libtoolize --copy --force && $automake --copy --add-missing --foreign && $autoconf
 
